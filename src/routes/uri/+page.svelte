87b753d336b4e79f0ea9701c6e3d44c6a236386a
A. Hassan <git@hassan.web.id>
2023-03-25 15:51:46 +0700

    feat: add encode and decode uri

--- src/routes/uri/+page.svelte
+++ src/routes/uri/+page.svelte
@@ -1,13 +1,31 @@
+<script lang="ts">
+	let source = '';
+	let result = '';
+
+	function onEncode() {
+		result = encodeURIComponent(source);
+	}
+	function onDecode() {
+		result = decodeURIComponent(source);
+	}
+</script>
+
 <div class="">
 	<h2 class="text-2xl mb-2">URI Tuls</h2>
 	<textarea
 		class="my-3 p-2 w-full h-40 block resize-y shadow-slate-900 border border-slate-200 rounded"
 		placeholder="Source"
+		bind:value={source}
 	/>
-	<button class="m-1 p-2 border rounded-md bg-emerald-500 text-white">Encode</button>
-	<button class="m-1 p-2 border rounded-md bg-emerald-500 text-white">Decode</button>
+	<button class="m-1 p-2 border rounded-md bg-emerald-500 text-white" on:click={onEncode}
+		>Encode</button
+	>
+	<button class="m-1 p-2 border rounded-md bg-emerald-500 text-white" on:click={onDecode}
+		>Decode</button
+	>
 	<textarea
 		class="my-3 p-2 w-full h-40 block resize-y shadow-slate-900 border border-slate-200 rounded"
 		placeholder="Result"
+		bind:value={result}
 	/>
 </div>
