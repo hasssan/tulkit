4ea813af72f7c98ed9607d92f2e9883a724f7998
A. Hassan <git@hassan.web.id>
2023-03-25 16:36:38 +0700

    feat: add date ISOString to Local Date Time
    using dayjs

--- src/routes/date/+page.svelte
+++ src/routes/date/+page.svelte
@@ -1,13 +1,19 @@
+<script lang="ts">
+	import dayjs from 'dayjs';
+
+	let sourceISO = '';
+	let resultISO = '';
+
+	$: resultISO = sourceISO ? dayjs(sourceISO).format('YY-MM-DD HH:mm:ss') : '';
+</script>
+
 <div class="">
 	<h2 class="text-2xl mb-2">Date Tuls</h2>
-	<textarea
-		class="my-3 p-2 w-full h-40 block resize-y shadow-slate-900 border border-slate-200 rounded"
-		placeholder="Source"
-	/>
-	<button class="m-1 p-2 border rounded-md bg-emerald-500 text-white">to ISOString</button>
-	<button class="m-1 p-2 border rounded-md bg-emerald-500 text-white">to Local Date</button>
-	<textarea
-		class="my-3 p-2 w-full h-40 block resize-y shadow-slate-900 border border-slate-200 rounded"
-		placeholder="Result"
-	/>
+	<h3>Date Time to ISOString</h3>
+	<input type="date" />
+	<input type="time" />
+	<input type="text" placeholder="Result" />
+	<h3>ISOString to Local Date Time</h3>
+	<input type="text" placeholder="2023-03-25T00:00:00.000Z" bind:value={sourceISO} />
+	<input type="text" placeholder="2023-03-25 07:00:00" bind:value={resultISO} />
 </div>
