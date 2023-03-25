fe1458ac4373f9296bef1b6f41d306f676171640
A. Hassan <git@hassan.web.id>
2023-03-25 15:09:44 +0700

    feat: add date page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
 <a href="/uri">uri</a>
 <a href="/string">string</a>
+<a href="/date">date</a>
---
+++ src/routes/date/+page.svelte
@@ -0,0 +1,13 @@
+<div class="">
+	<h2 class="text-2xl mb-2">Date Tuls</h2>
+	<textarea
+		class="my-3 p-2 w-full h-40 block resize-y shadow-slate-900 border border-slate-200 rounded"
+		placeholder="Source"
+	/>
+	<button class="m-1 p-2 border rounded-md bg-emerald-500 text-white">to ISOString</button>
+	<button class="m-1 p-2 border rounded-md bg-emerald-500 text-white">to Local Date</button>
+	<textarea
+		class="my-3 p-2 w-full h-40 block resize-y shadow-slate-900 border border-slate-200 rounded"
+		placeholder="Result"
+	/>
+</div>
