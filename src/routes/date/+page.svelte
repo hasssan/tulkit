31a9265b8a9223c7bcf556b41040d0c635342813
A. Hassan <git@hassan.web.id>
2023-05-20 19:41:38 +0700

    feat: Date to ISOString

--- src/routes/date/+page.svelte
+++ src/routes/date/+page.svelte
@@ -4,15 +4,28 @@
 	let sourceISO = '';
 	let resultISO = '';
 
-	$: resultISO = sourceISO ? dayjs(sourceISO).format('YY-MM-DD HH:mm:ss') : '';
+	$: resultISO = sourceISO ? dayjs(sourceISO).format('YYYY-MM-DD HH:mm:ss') : '';
+
+	let dateSource = '';
+	let timeSource = '';
+	let dateResult = '';
+
+	$: dateSource, timeSource, processDateResult();
+
+	function processDateResult() {
+		if (!dateSource && timeSource) {
+			dateSource = dayjs().format('YYYY-MM-DD')
+		}
+		dateResult = dateSource || timeSource ? dayjs(`${dateSource} ${timeSource}`).toISOString() : '';
+	}
 </script>
 
 <div class="">
 	<h2 class="text-2xl mb-2">Date Tuls</h2>
 	<h3>Date Time to ISOString</h3>
-	<input type="date" />
-	<input type="time" />
-	<input type="text" placeholder="Result" />
+	<input type="date" bind:value={dateSource} />
+	<input type="time" bind:value={timeSource} />
+	<input type="text" placeholder="Result" bind:value={dateResult} />
 	<h3>ISOString to Local Date Time</h3>
 	<input type="text" placeholder="2023-03-25T00:00:00.000Z" bind:value={sourceISO} />
 	<input type="text" placeholder="2023-03-25 07:00:00" bind:value={resultISO} />
