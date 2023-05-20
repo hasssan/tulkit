3391385aa76e111501f698c9f13b34a1c98f6023
A. Hassan <git@hassan.web.id>
2023-05-20 19:37:22 +0700

    feat: add beta notice

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,7 @@
 <div class="container mx-auto my-4">
 	<h1 class="text-4xl font-bold mb-4">
 		<a href="/">Tulkit</a>
+		<span class="text-sm"><sub>v2 beta</sub></span>
 	</h1>
 	<NavigationComponent />
 	<slot />
