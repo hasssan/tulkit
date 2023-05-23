3f91a3e5cbfa6d3f2f573715d1c1ff4b56f06eed
A. Hassan <git@hassan.web.id>
2023-05-23 21:38:45 +0700

    fix: url in logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script>
 	import '../app.css';
 	import NavigationComponent from '../components/navigation.svelte';
+	import { base } from '$app/paths';
 </script>
 
 <div class="container mx-auto my-4">
 	<h1 class="text-4xl font-bold mb-4">
-		<a href="/">Tulkit</a>
+		<a href="{base}/">Tulkit</a>
 		<span class="text-sm"><sub>v2 beta</sub></span>
 	</h1>
 	<NavigationComponent />
