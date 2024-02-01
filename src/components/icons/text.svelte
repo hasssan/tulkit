77a535a67527f48e6962e0ba6378f2d7cc9b0ef6
A. Hassan <git@hassan.web.id>
2024-02-01 23:50:34 +0700

    feat: complete redesign

---
+++ src/components/icons/text.svelte
@@ -0,0 +1,16 @@
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	width="24"
+	height="24"
+	viewBox="0 0 24 24"
+	fill="none"
+	stroke="currentColor"
+	stroke-width="2"
+	stroke-linecap="round"
+	stroke-linejoin="round"
+	class="h-16 w-16 mb-4 text-[rgb(16,185,129)] dark:text-[rgb(16,185,129)]"
+>
+	<path d="M17 6.1H3"></path>
+	<path d="M21 12.1H3"></path>
+	<path d="M15.1 18H3"></path>
+</svg>
--- src/routes/date/+page.svelte
+++ src/routes/date/+page.svelte
@@ -22,15 +22,77 @@
 	}
 </script>
 
-<div class="">
-	<h2 class="text-2xl mb-2">Date Tuls</h2>
-	<h3>Date Time to ISOString</h3>
-	<input type="date" bind:value={dateSource} />
-	<input type="time" bind:value={timeSource} />
-	<input type="text" placeholder="Result" bind:value={dateResult} />
-	<CopyButton class={copyButtonStyle} text={dateResult} />
-	<h3>ISOString to Local Date Time</h3>
-	<input type="text" placeholder="2023-03-25T00:00:00.000Z" bind:value={sourceISO} />
-	<input type="text" placeholder="2023-03-25 07:00:00" bind:value={resultISO} />
-	<CopyButton class={copyButtonStyle} text={resultISO} />
-</div>
+<main class="flex-1 py-12 bg-white dark:bg-gray-800">
+	<div class="container px-6 mx-auto grid grid-cols-2 gap-8">
+		<div class="flex flex-col items-center p-6 bg-white rounded-lg shadow-md dark:bg-gray-900">
+			<h2 class="text-2xl font-bold text-[rgb(16,185,129)] dark:text-[rgb(16,185,129)] mb-4">
+				Date Time to ISOString
+			</h2>
+			<form class="w-full">
+				<label
+					class="block text-gray-700 dark:text-gray-400 text-sm font-bold mb-2"
+					for="inputData"
+				>
+					Input
+				</label>
+				<div class="flex">
+					<input
+						type="date"
+						class="mr-1 h-10 border-input bg-background text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 shadow appearance-none border rounded py-2 px-3 text-gray-700 dark:text-gray-400 leading-tight focus:outline-none focus:shadow-outline mb-4"
+						bind:value={dateSource}
+					/>
+					<input
+						type="time"
+						class="ml-1 h-10 border-input bg-background text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 shadow appearance-none border rounded py-2 px-3 text-gray-700 dark:text-gray-400 leading-tight focus:outline-none focus:shadow-outline mb-4"
+						bind:value={timeSource}
+					/>
+				</div>
+				<label
+					class="block text-gray-700 dark:text-gray-400 text-sm font-bold mb-2"
+					for="outputData"
+				>
+					Output
+				</label><textarea
+					id="outputData"
+					class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-400 leading-tight focus:outline-none focus:shadow-outline"
+					placeholder="Your result will appear here..."
+					readonly
+					bind:value={dateResult}
+				></textarea>
+				<CopyButton text={dateResult} />
+			</form>
+		</div>
+		<div class="flex flex-col items-center p-6 bg-white rounded-lg shadow-md dark:bg-gray-900">
+			<h2 class="text-2xl font-bold text-[rgb(16,185,129)] dark:text-[rgb(16,185,129)] mb-4">
+				ISOString to Local Date Time
+			</h2>
+			<form class="w-full">
+				<label
+					class="block text-gray-700 dark:text-gray-400 text-sm font-bold mb-2"
+					for="inputData"
+				>
+					Input
+				</label>
+				<input
+					type="text"
+					class="w-full h-10 border-input bg-background text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 shadow appearance-none border rounded py-2 px-3 text-gray-700 dark:text-gray-400 leading-tight focus:outline-none focus:shadow-outline mb-4"
+					placeholder="2023-03-25T00:00:00.000Z"
+					bind:value={sourceISO}
+				/>
+				<label
+					class="block text-gray-700 dark:text-gray-400 text-sm font-bold mb-2"
+					for="outputData"
+				>
+					Output
+				</label><textarea
+					id="outputData"
+					class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-400 leading-tight focus:outline-none focus:shadow-outline"
+					placeholder="Your result will appear here..."
+					readonly
+					bind:value={resultISO}
+				></textarea>
+				<CopyButton text={resultISO} />
+			</form>
+		</div>
+	</div>
+</main>
